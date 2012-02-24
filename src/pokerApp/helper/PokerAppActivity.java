7841f67d2c6c50f8bcffce9c811efddf362f9acf
Jonas <Jonaseikli@gmail.com>
2012-02-24 15:44:08 +0100

    started project

---
+++ src/pokerApp/helper/PokerAppActivity.java
@@ -0,0 +1,16 @@
+package pokerApp.helper;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class PokerAppActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TextView tv = new TextView(this);
+        tv.setText("Hello, Android");
+        setContentView(tv);
+    }
+}
